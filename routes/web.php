d66bf424f81f2a76a84cc314d901f48e427a793b
bethesida <idongesitutong@gmail.com>
2023-12-15 10:20:38 +0100

    added roles and permissions with spatie

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +30,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Our resource routes
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
 });
+
+
+Route::domain('{domain}.wildcard.test')->group(function () {
+    Route::get('/domain', [ProgramController::class, 'index'])->name('program');
+});
+
+Route::get('/create', [ProgramController::class, 'createForm'])->name('create.form');
+Route::post('/create', [ProgramController::class, 'createProgram'])->name('create.program');
 
 require __DIR__.'/auth.php';
